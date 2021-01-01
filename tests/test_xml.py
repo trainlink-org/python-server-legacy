c961ea247a38f6b33bfb3e700c28ca4c79e6c1bd
Matt <matt.hu1@outlook.com>
2021-01-01 20:08:29 +0000

    add more xml tests

--- tests/test_xml.py
+++ tests/test_xml.py
@@ -1,8 +1,20 @@
 import trainlinkUtils
 
 
-def test_answer():
+def test_normal():
     utils = trainlinkUtils.xmlUtils('config/config.default.xml')
     assert utils.loadXml() != 1
     assert utils.listCabs() == {'Train1':'1','Train2':'2'}
     assert utils.loadConfig() == {'ipAddress': '0.0.0.0', 'port': '6789', 'serialPort': '/dev/ttyUSBx', 'debug': 'True'}
+
+def test_alternative():
+    utils = trainlinkUtils.xmlUtils('config/config.default.xml')
+    utils.loadXml()
+    utils.xmlFile['config']['server']['ip'] = 'local'
+    utils.xmlFile['config']['cabs']['cab'].pop(1)
+    assert utils.listCabs() == {'Train1':'1'}
+    assert utils.loadConfig() == {'ipAddress': '0.0.0.0', 'port': '6789', 'serialPort': '/dev/ttyUSBx', 'debug': 'True'}
+    
+def test_erroneous():
+    utils = trainlinkUtils.xmlUtils('nonexistant.xml')
+    assert utils.loadXml() == 1
