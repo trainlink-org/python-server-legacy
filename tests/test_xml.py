d91c9696eda829f5fda73102302c1d08d6ff2ce3
Matt <matt.hu1@outlook.com>
2021-01-01 12:02:20 +0000

    Add more tests for xml

--- tests/test_xml.py
+++ tests/test_xml.py
@@ -4,3 +4,5 @@ import trainlinkUtils
 def test_answer():
     utils = trainlinkUtils.xmlUtils('config/config.default.xml')
     assert utils.loadXml() != 1
+    assert utils.listCabs() == {'Train1':'1','Train2':'2'}
+    assert utils.loadConfig() == {'ipAddress': '0.0.0.0', 'port': '6789', 'serialPort': '/dev/ttyUSBx', 'debug': 'True'}
