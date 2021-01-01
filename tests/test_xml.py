03c826f389f3954751e223bac2188fd3c975dbf6
Matt <matt.hu1@outlook.com>
2021-01-01 20:11:40 +0000

    fix test_xml.py

--- tests/test_xml.py
+++ tests/test_xml.py
@@ -13,7 +13,7 @@ def test_alternative():
     utils.xmlFile['config']['server']['ip'] = 'local'
     utils.xmlFile['config']['cabs']['cab'].pop(1)
     assert utils.listCabs() == {'Train1':'1'}
-    assert utils.loadConfig() == {'ipAddress': '0.0.0.0', 'port': '6789', 'serialPort': '/dev/ttyUSBx', 'debug': 'True'}
+    assert utils.loadConfig() == {'ipAddress': '127.0.0.1', 'port': '6789', 'serialPort': '/dev/ttyUSBx', 'debug': 'True'}
     
 def test_erroneous():
     utils = trainlinkUtils.xmlUtils('nonexistant.xml')
