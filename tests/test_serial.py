15b005aca0521b36afb734f02f86b743cde12cc7
Matt <matt.hu1@outlook.com>
2021-01-02 19:54:21 +0000

    fix serial tests

--- tests/test_serial.py
+++ tests/test_serial.py
@@ -2,6 +2,6 @@ import trainlinkSerial
 import asyncio
 
 def test_serial():
-    serial = trainlinkSerial.comms()
+    serial = trainlinkSerial.comms('/dev/ttyNonexistant')
     serial.updateCabs([0,0],[1,1])
     serial.directCommand("<nonexistant>")
