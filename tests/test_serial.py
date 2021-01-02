9891e15459da5d222f2fb544d52d3e436b46da16
Matt <matt.hu1@outlook.com>
2021-01-02 20:02:06 +0000

    Add more serial tests

--- tests/test_serial.py
+++ tests/test_serial.py
@@ -4,4 +4,9 @@ import asyncio
 def test_serial():
     serial = trainlinkSerial.comms('/dev/ttyNonexistant')
     serial.updateCabs([0,0],[1,1])
-    serial.directCommand("<nonexistant>")
+    asyncio.get_event_loop().run_until_complete(serial.directCommand("<nonexistant>"))
+    asyncio.get_event_loop().run_until_complete(serial.setPower(0))
+    asyncio.get_event_loop().run_until_complete(serial.setPower(1))
+    asyncio.get_event_loop().run_until_complete(serial.setPower(2))
+    asyncio.get_event_loop().run_until_complete(serial.setPower('not a int'))
+    asyncio.get_event_loop().run_until_complete(serial.setFunction(functionStates=[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]))
