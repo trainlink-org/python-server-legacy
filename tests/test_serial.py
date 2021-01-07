a76a219be0f7827c759d04175161405770f90c07
Matt <matt.hu1@outlook.com>
2021-01-07 20:23:50 +0000

    Fix tests

--- tests/test_serial.py
+++ tests/test_serial.py
@@ -1,8 +1,9 @@
 import trainlinkSerial
-import asyncio
+import asyncio, pyaddons
 
 def test_serial():
-    serial = trainlinkSerial.comms('/dev/ttyNonexistant')
+    logfile = pyaddons.logger()
+    serial = trainlinkSerial.comms('/dev/ttyNonexistant',logfile)
     serial.updateCabs([0,0],[1,1])
     asyncio.get_event_loop().run_until_complete(serial.directCommand("<nonexistant>"))
     asyncio.get_event_loop().run_until_complete(serial.setPower(0))
