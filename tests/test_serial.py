25d9a4c7860feadbeac07c486a2e5da61389f64c
Matt <matt.hu1@outlook.com>
2021-01-24 14:43:00 +0000

    add second cab to tests

--- tests/test_serial.py
+++ tests/test_serial.py
@@ -5,7 +5,7 @@ import asyncio, pyaddons
 def test_serial():
     logfile = pyaddons.logger()
     serial = trainlinkSerial.comms('/dev/ttyNonexistant',logfile)
-    serial.updateCabs(trainlinkObjects.cab("train",1))
+    serial.updateCabs(trainlinkObjects.cab("train",1),trainlinkObjects.cab("train2", 2))
 #    serial.updateCabs([0,0],[1,1])
     asyncio.get_event_loop().run_until_complete(serial.directCommand("<nonexistant>"))
     asyncio.get_event_loop().run_until_complete(serial.setPower(0))
